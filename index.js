eb910f9f998993adc2faacad81d5468c922b6b94
zuulaxx <guigui.8929@gmail.com>
2022-12-08 20:17:08 +0100

    Update

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+var timestamp = Date.now();
+const date = new Date(timestamp);
+const item = document.createElement('h5');
+item.innerHTML = `<span class="date">Le ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} à ${date.getHours()}:${date.getMinutes()}</span><br />`
