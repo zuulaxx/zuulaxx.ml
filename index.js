e2c47e0aa7a7f6e7340a58c0729d9cc9d323be94
zuulaxx <guigui.8929@gmail.com>
2022-12-11 13:44:10 +0100

    Update

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var timestamp = Date.now();
 const date = new Date(timestamp);
-`Le ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} à ${date.getHours()}:${date.getMinutes()}`
+document.getElementById('current-date').innerHTML = `Le ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} à ${date.getHours()}:${date.getMinutes()}`
